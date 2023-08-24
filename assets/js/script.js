902f284abd271111bd2c444641f671edef9b77f4
ignasvilkas <ignas.pilybas@icloud.com>
2023-08-24 10:15:52 +0100

    add the rest of const and Event Listener to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
 startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', setNextQuestion);
 
 function startGame() {
     startButton.classList.add('hide');
